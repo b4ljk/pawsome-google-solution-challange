3de2dc550f431c8de90d5a41b5025feff56460a7
Baljinnyam Dayan <baljinnyam.dayan@gmail.com>
2023-03-20 21:14:00 +0800

    massive changes

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -49,9 +46,19 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDpIOw6EVXd09vYspd6wP-5GLseTNjdwqM',
+    appId: '1:960488407399:web:1afb73976b25469677afc2',
+    messagingSenderId: '960488407399',
+    projectId: 'pawsome-59b88',
+    authDomain: 'pawsome-59b88.firebaseapp.com',
+    storageBucket: 'pawsome-59b88.appspot.com',
+    measurementId: 'G-H3ZWTSTWMW',
+  );
+
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyCPpC1KtIlupw6S5x3kusouZ3u6LueeNuQ',
-    appId: '1:960488407399:android:b11046638e0e498377afc2',
+    appId: '1:960488407399:android:d2485180f6ed080b77afc2',
     messagingSenderId: '960488407399',
     projectId: 'pawsome-59b88',
     storageBucket: 'pawsome-59b88.appspot.com',
@@ -59,12 +66,12 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyBhCqe8Y7CF73f7FztW-GGBBZ3EBuyIv7k',
-    appId: '1:960488407399:ios:044c69833c9e2c9b77afc2',
+    appId: '1:960488407399:ios:806f0907bee6911a77afc2',
     messagingSenderId: '960488407399',
     projectId: 'pawsome-59b88',
     storageBucket: 'pawsome-59b88.appspot.com',
     androidClientId: '960488407399-5amcsbv4sm8aon3rehtd5lkh07vukvko.apps.googleusercontent.com',
-    iosClientId: '960488407399-9shbovmrocmkbvnaj2bf5aj0pkhpt1i0.apps.googleusercontent.com',
-    iosBundleId: 'com.example.bestFlutterUiTemplates',
+    iosClientId: '960488407399-qipdbu181jpvnl03p8rhgdppvu8t1v3r.apps.googleusercontent.com',
+    iosBundleId: 'com.example.pawsome',
   );
 }
