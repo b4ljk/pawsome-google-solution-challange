0976ee3b7251ff63c981254b7fbdae31b8528ac8
Baljinnyam Dayan <baljinnyam.dayan@gmail.com>
2023-04-01 19:43:25 +0800

    remove useless stuff

--- lib/pawsome/screens/info_screen/post_list_screen/widget/blog_detail/main.dart
+++ lib/pawsome/screens/info_screen/post_list_screen/widget/blog_detail/main.dart
@@ -19,7 +19,6 @@ class AnimalCareScreen extends StatelessWidget {
                 Image.network(
                   'https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B900%2C533%5D&w=2000&h=1333&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F03%2F12%2Fpomeranian-white-puppy-921029690-2000.jpg',
                   fit: BoxFit.cover,
-                  height: 200.0,
                   width: double.infinity,
                 ),
                 SizedBox(height: 16.0),
